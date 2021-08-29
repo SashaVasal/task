58ca9731a0fd35a962fb53ebfce5424b194d330b
SashaVasal <34645577+SashaVasal@users.noreply.github.com>
2021-08-29 21:26:31 +1000

    2

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -14,42 +14,26 @@ public:
             m[i][i] = 1;
         }
 
-        for (int i = 1; i < size; i++) {
-            for (int j = 1; j < size; j++) {
-                m[i][j] = m[i][j - 1] * i + m[i - 1][j - 1];
+        for (int i = size - 2; i >= 0 ; i--) {
+            for (int j = size - 2; j >= 0; j--) {
+                m[i][j] = m[i][j + 1] * (size-i-1) + m[i + 1][j + 1];
             }
         }
     }
 
     void show_matrix() {
-        for (int i = size-1; i >= 0; i--) {
-            for (int j = size - 1; j >= 0; j--) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 cout << m[i][j] << " ";
             }
             cout << "\n";
         }
     }
     
-    void show_pattern() {
-        for (int i = 0; i < size; i++) {
-            cout << "b_" << 6 - i << " = ";
-            for (int j = 0; j < size; j++) {
-                if (m[i][j] != 0) {
-                    if (m[i][j] != 1 || j == 6) {
-                        cout << m[i][j] << " ";
-                    }
-                    if (j != 6) {
-                        cout << "a_" << 6 - j;
-                       if (j != 0) {
-                           cout << " + ";
-                       }
-                    }                 
-                }   
-            }
-
-            cout << "\n";
-        }
+    vector<vector<int>> get_matrix() {
+        return m;
     }
+    
 private:
     vector<vector<int>> m;
     int size = 0;
@@ -57,6 +41,6 @@ private:
 
 int main() {
     mymatrix *m = new mymatrix(6);
-    m->show_pattern();
+    m->show_matrix();
     return 0;
 }
